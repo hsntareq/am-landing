e01b732800762c52b50a568a3039be20e74a901e
Hasan Tareq <hsntareq@gmail.com>
2024-01-24 23:54:36 +0600

    Video modal added after body

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('Hello, world!');
+
+let videoModalContent = (videoURL) => {
+	return `<div class="modal-wrap"> <div class="modal-content"> <span class="material-symbols-outlined modal-close"> cancel </span> <div class="video-container"> <iframe width="100%" height="100%" src="${videoURL}" frameborder="0" allowfullscreen></iframe> </div> </div> </div>`;
+	// https://www.youtube.com/embed/mUGYPlAgJPw
+}
+
+document.querySelector('[data-modal]').addEventListener('click', (e) => {
+	e.preventDefault();
+	document.body.insertAdjacentHTML('afterbegin', videoModalContent(e.target.parentElement.dataset.modal));
+	document.querySelector('.modal-wrap').classList.add('show');
+
+	setTimeout(() => {
+		document.querySelector('.modal-wrap').classList.add('showvideo');
+	}, 10);
+
+	document.querySelector('.modal-close, .modal-wrap').addEventListener('click', () => {
+		document.querySelector('.modal-wrap').classList.remove('showvideo');
+		setTimeout(() => {
+			document.querySelector('.modal-wrap').remove();
+		}, 100);
+	});
+});
