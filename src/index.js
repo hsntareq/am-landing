4a5a7827d8d7995c68b120109979cd5703a2a68c
Hasan Tareq <hsntareq@gmail.com>
2024-01-26 19:00:24 +0600

    Mixin files are separated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-console.log('Hello, world!');
-
+/**
+ * Script for the amapi plugin.
+ *
+ * @param {string} videoURL
+ * @returns {string} videoModalContent
+ */
 let videoModalContent = (videoURL) => {
 	return `<div class="modal-wrap"> <div class="modal-content"> <span class="material-symbols-outlined modal-close"> cancel </span> <div class="video-container"> <iframe width="100%" height="100%" src="${videoURL}" frameborder="0" allowfullscreen></iframe> </div> </div> </div>`;
 }
@@ -7,12 +11,17 @@ let videoModalContent = (videoURL) => {
 document.querySelector('[data-modal]').addEventListener('click', (e) => {
 	e.preventDefault();
 	document.body.insertAdjacentHTML('afterbegin', videoModalContent(e.target.parentElement.dataset.modal));
-	document.querySelector('.modal-wrap').classList.add('show');
+
+
+
+	setTimeout(() => {
+		document.querySelector('.modal-wrap').classList.add('show');
+	}, 100);
 
 	setTimeout(() => {
 		document.querySelector('.modal-wrap').classList.add('showvideo');
 		document.body.style.overflow = 'hidden';
-	}, 10);
+	}, 100);
 
 	document.querySelector('.modal-close, .modal-wrap').addEventListener('click', () => {
 		document.querySelector('.modal-wrap').classList.remove('showvideo');
